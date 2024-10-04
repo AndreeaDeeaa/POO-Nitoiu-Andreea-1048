2f267e814d7ac67ba4b844ec9fad7d806f3c09b5
Nitoiu Andreea <156124937+AndreeaDeeaa@users.noreply.github.com>
2024-10-05 01:59:16 +1200

    Add files via upload

---
+++ suma_a_doua_numere.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a, b, suma;
+    cout<<"Introduceti primul numar:";
+    cin>>a;
+    cout<<"Introduceti al doilea numar numar:";
+    cin>>b;
+    suma=a+b;
+    cout<<"Suma celor doua numere este egala cu:"<<suma<<endl;
+    return 0;
+}
